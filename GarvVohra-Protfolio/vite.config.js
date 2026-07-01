fbc1c9a5dabe5ede752b97ed658c1b6a749c6380
Garv Vohra <gvoh0002@student.monash.edu>
2026-07-02 05:19:01 +1000

    added build files

--- GarvVohra-Protfolio/vite.config.js
+++ GarvVohra-Protfolio/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'build',
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    css: true,
-  },
-});
+  base: '/GarvVohra-Protfolio/',   // MUST match your repo name exactly
+})
